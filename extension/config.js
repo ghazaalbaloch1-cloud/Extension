0f2fba3a2f54a892a055929295a7a030c98acd1f
ghazaalbaloch1-cloud <ghazaalbaloch1@gmail.com>
2026-08-21 02:08:28 +0500

    Add Chrome extension OAuth configuration template

---
+++ extension/config.js
@@ -0,0 +1,3 @@
+// This is a public Chrome Extension OAuth client ID, not a client secret.
+// Create a Google Cloud OAuth client of type "Chrome Extension" and paste the ID here.
+const EXTENSION_GOOGLE_CLIENT_ID = 'REPLACE_WITH_CHROME_EXTENSION_CLIENT_ID.apps.googleusercontent.com';
