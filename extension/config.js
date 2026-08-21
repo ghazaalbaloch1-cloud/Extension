528933145760cf02a10991c320f0ccc165f42f69
ghazaalbaloch1-cloud <ghazaalbaloch1@gmail.com>
2026-08-21 21:27:59 +0500

    fix: expose OAuth client config to MV3 worker

--- extension/config.js
+++ extension/config.js
@@ -2,4 +2,7 @@
 // This must be the OAuth 2.0 client ID created in Google Cloud with
 // application type "Chrome Extension" for this exact extension ID.
 // Never put a Google client secret in this file.
-const EXTENSION_GOOGLE_CLIENT_ID = 'REPLACE_WITH_CHROME_EXTENSION_CLIENT_ID.apps.googleusercontent.com';
+//
+// Use `var` because this file is loaded by importScripts() into the MV3
+// service-worker global scope before background.js is evaluated.
+var EXTENSION_GOOGLE_CLIENT_ID = 'REPLACE_WITH_CHROME_EXTENSION_CLIENT_ID.apps.googleusercontent.com';
